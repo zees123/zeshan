8df83f40bea038e1b97e5e64f5ab07dececd41a9
muhammad <muhammad@192.168.10.42>
2016-06-19 11:37:52 -0700

    its second

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
+	/**
+	 * 
+	 * @param args
+	 * <h1>this is first heading</h1>
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int l=0;
+System.out.println("hello word");
 	}
 
 }
